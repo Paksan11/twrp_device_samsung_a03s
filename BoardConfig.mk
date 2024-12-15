c7c64ae0df466990b14a2a15b5459cc924f692c4
Paksan11 <87140907+Paksan11@users.noreply.github.com>
2024-12-15 07:04:02 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_NO_RADIOIMAGE := true
 # Kernel
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00080000 --ramdisk_offset 0x11B00000 --tags_offset 0x07880000 --header_version 2 --dtb_offset 0x07880000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00080000 --ramdisk_offset 0x11B00000 --tags_offset 0x07880000 --header_version 2 --dtb_offset 0x07880000 --recovery_dtbo $(BOARD_PREBUILT_DTBOIMAGE)
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
