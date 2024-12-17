c6241549c52cbd81a638eddd781367559b14c6c9
Paksan11 <87140907+Paksan11@users.noreply.github.com>
2024-12-18 05:51:38 +0700

    Update twrp_a03s.mk

--- twrp_a03s.mk
+++ twrp_a03s.mk
@@ -22,16 +22,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common TWRP stuff.
+# For PitchBlack use vendor/pb/config/common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
-$(call find-copy-subdir-files,*,$(DEVICE_PATH)/device.mk)
+$(call inherit-product, device/samsung/a03s/device.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files, device/samsung/a03s/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a03s/recovery/root,recovery/root)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := twrp_a03s
@@ -39,4 +40,4 @@ PRODUCT_DEVICE := a03s
 PRODUCT_MODEL := Galaxy A03s
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
