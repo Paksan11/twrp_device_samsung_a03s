c0e7329626b23600a1f10731113a0e6928d3c376
Paksan11 <87140907+Paksan11@users.noreply.github.com>
2025-01-01 08:52:54 +0700

    Device Tree for Galaxy A03s

---
+++ device.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 33
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
