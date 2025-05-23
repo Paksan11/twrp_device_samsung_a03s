20c08367213a5bbb90f68fc18abe7a9f1f95dcdf
Topser9 <87140907+Paksan11@users.noreply.github.com>
2025-05-24 01:50:14 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -32,18 +32,3 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0
-
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.0 \
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0
-
-# Keystore2
-PRODUCT_PACKAGES += \
-    android.system.keystore2
